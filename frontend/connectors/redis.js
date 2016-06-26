95a43485d05111f92b812044990c456226c7e162
urahiroshi <asj.hiroshi@gmail.com>
2016-06-26 23:03:14 +0900

    Use ES2015 class
    const/let variable doesn't have prototype property.

--- frontend/connectors/redis.js
+++ frontend/connectors/redis.js
@@ -1,22 +1,25 @@
 const redis = require('redis');
 
-const Connector = (options) => {
-  this._client = redis.createClient(options);
-};
+class Connector {
+  constructor(options) {
+    this._client = redis.createClient(options);
+  }
 
-Connector.prototype.get = (key) => {
-  json = this._client.get(key);
-  return JSON.parse(json);
-};
+  get(key) {
+    json = this._client.get(key);
+    return JSON.parse(json);
+  }
 
-Connector.prototype.set = (key, value) => {
-  json = JSON.stringify(value);
-  this._client.set(key, json);
-};
+  set(key, value) {
+    json = JSON.stringify(value);
+    this._client.set(key, json);
+  }
 
-Connector.prototype.close = () => {
-  this._client.end(true);
-};
+  close() {
+    this._client.end(true);
+  }
+}
+
 
 ['multi', 'exec'].forEach((method) => {
   Connector.prototype[method] = () => {
--- frontend/models/usecase.js
+++ frontend/models/usecase.js
@@ -5,34 +5,44 @@ function createKey() {
   return crypto.randomBytes(8).toString('base64');
 }
 
-const Usecase = (params) => {
-  this.params = {
-    url: params.url || '',
-    flow: params.flow || [],
-    timeout: params.timeout || 0,
-    validation: params.validation || [],
-    browser: params.browser || {}
-  };
-};
+class Usecase {
+  constructor(params) {
+    this.params = {
+      url: params.url || '',
+      flow: params.flow || [],
+      timeout: params.timeout || 0,
+      validation: params.validation || [],
+      browser: params.browser || {}
+    };
+  }
 
-Usecase.find = (key) => {
-  const connector = new Connector();
-  const params = connector.get(key);
-  const usecase = new Usecase(params);
-  usecase.key = key;
-  return usecase;
-};
+  save() {
+    const connector = new Connector();
+    const key = this.key || createKey();
+    connector.multi()
+    .set(key, this.params)
+    .set('usecases', key)
+    .exec((err, replies) => {
+      console.log(err, replies);
+      connector.close();
+    });
+  }
 
-Usecase.prototype.save = () => {
-  const connector = new Connector();
-  const key = this.key || createKey();
-  connector.multi()
-  .set(key, this.params)
-  .set('usecases', key)
-  .exec((err, replies) => {
-    console.log(err, replies);
-    connector.close();
-  })
-};
+  static find(key) {
+    const connector = new Connector();
+    const params = connector.get(key);
+    const usecase = new Usecase(params);
+    usecase.key = key;
+    return usecase;
+  }
+
+  static findAll() {
+    const connector = new Connector();
+    const keys = connector.get('usecases');
+    return keys.map((key) => {
+      return connector.get(key);
+    });
+  }
+}
 
 module.exports = Usecase;
