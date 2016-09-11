b231ca393732f4a182ade140770d2ff32fd3ce6c
urahiroshi <asj.hiroshi@gmail.com>
2016-09-12 00:17:32 +0900

    Fix parameters by table

--- gui/javascripts.src/views/organisms/parameters.jsx
+++ gui/javascripts.src/views/organisms/parameters.jsx
@@ -3,27 +3,45 @@ import _ from 'lodash';
 
 import Heading from '../atoms/heading.jsx';
 import Button from '../atoms/button.jsx';
-import KeyValue from '../molecules/key-value.jsx';
-import KeyTable from '../organisms/key-table.jsx';
+import Table from '../organisms/table.jsx';
+
+const style = {
+  table: {},
+  tr: {
+    verticalAlign: 'top',
+    borderTop: '1px #CCCCCC solid',
+  },
+  th: {
+    padding: '10px 10px',
+  },
+  td: {
+    padding: '10px 0px',
+  },
+};
 
 const Parameters = ({ keyValues }) =>
   <div>
     <Heading value="Parameters" />
-    {
-      _.map(keyValues, (value, key) => {
-        if (_.isObject(value)) {
+    <table style={style.table}>
+      <tbody>
+      {
+        _.map(keyValues, (value, key) => {
+          let cell;
+          if (_.isObject(value)) {
+            cell = <Table header={value.header} rows={value.rows} />;
+          } else {
+            cell = value;
+          }
           return (
-            <KeyTable
-              key={key}
-              heading={key}
-              tableHeader={value.header}
-              tableRows={value.rows}
-            />
+            <tr key={key} style={style.tr}>
+              <th style={style.th}>{key}</th>
+              <td style={style.td}>{cell}</td>
+            </tr>
           );
-        }
-        return <KeyValue key={key} heading={key} value={value} />;
-      })
-    }
+        })
+      }
+      </tbody>
+    </table>
     <Button label="edit" />
   </div>;
 
