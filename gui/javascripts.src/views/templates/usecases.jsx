39cf77da47d0013469e9033463031431262c0fc2
urahiroshi <asj.hiroshi@gmail.com>
2017-01-17 22:03:52 +0900

    Fix usecases to simple list

--- gui/javascripts.src/views/templates/usecases.jsx
+++ gui/javascripts.src/views/templates/usecases.jsx
@@ -1,38 +1,34 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
-import UsecaseTable from '../organisms/table.jsx';
 import AddUsecaseButton from '../atoms/button.jsx';
 import NewUsecase from '../containers/new-usecase';
 import Modal from '../containers/modal';
 import { API_NAME } from '../../consts';
 
-const Usecases = ({ usecases, onClickNewUsecaseButton }) => {
-  const header = [
-    'ID', 'Usecase', 'Create Date Time',
-  ];
-  const rows = usecases.map((usecase) =>
-    [
-      <Link to={`/usecases/${usecase.id}`}>{usecase.id}</Link>,
-      usecase.name,
-      (new Date(usecase.createdAt)).toLocaleString(),
-    ]
-  );
-  return (
-    <div>
-      <AddUsecaseButton
-        label="Add"
-        onClick={onClickNewUsecaseButton}
-      />
-      <UsecaseTable header={header} rows={rows} />
-      <Modal
-        name={API_NAME.ADD_USECASE}
-        title="New Usecase"
-      >
-        <NewUsecase />
-      </Modal>
-    </div>
-  );
-};
+const Usecases = ({ usecases, onClickNewUsecaseButton }) => (
+  <div>
+    <AddUsecaseButton
+      label="Add"
+      onClick={onClickNewUsecaseButton}
+    />
+    <ul>
+      {
+        usecases.map((usecase) =>
+          <li key={usecase.id}>
+            <Link to={`/usecases/${usecase.id}`}>{usecase.name}</Link>
+            <span> {(new Date(usecase.createdAt)).toLocaleString()}</span>
+          </li>
+        )
+      }
+    </ul>
+    <Modal
+      name={API_NAME.ADD_USECASE}
+      title="New Usecase"
+    >
+      <NewUsecase />
+    </Modal>
+  </div>
+);
 
 Usecases.propTypes = {
   usecases: PropTypes.array.isRequired,
