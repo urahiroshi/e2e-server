8ac3bdbcb532e3f7211c67daa53ec83cc65fe0df
urahiroshi <asj.hiroshi@gmail.com>
2017-01-17 22:41:01 +0900

    Add redux-logger

--- gui/javascripts.src/index.jsx
+++ gui/javascripts.src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
+import createLogger from 'redux-logger';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -11,10 +12,11 @@ import AppRouter from './router.jsx';
 import Reducers from './reducers';
 import Sagas from './sagas';
 
+const logger = createLogger();
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   Reducers,
-  applyMiddleware(sagaMiddleware)
+  applyMiddleware(sagaMiddleware, logger)
 );
 sagaMiddleware.run(Sagas);
 
