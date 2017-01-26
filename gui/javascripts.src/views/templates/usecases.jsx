4023eb207fffa94ca6795bed43af27a19a9efe5d
urahiroshi <asj.hiroshi@gmail.com>
2017-01-27 00:19:55 +0900

    Update usecases design

--- gui/javascripts.src/views/templates/usecases.jsx
+++ gui/javascripts.src/views/templates/usecases.jsx
@@ -1,6 +1,5 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
-import AddUsecaseButton from '../atoms/button.jsx';
 import Usecase from '../containers/usecase';
 import NewUsecase from '../containers/new-usecase';
 import EditUsecase from '../containers/edit-usecase';
@@ -11,20 +10,36 @@ import { API_NAME } from '../../consts';
 const Usecases = ({ usecases, selectedUsecaseId, onClickNewUsecaseButton }) => (
   <div className="row">
     <div className={(selectedUsecaseId) ? 'col-md-4' : 'col-md-12'} >
-      <AddUsecaseButton
-        label="Add"
-        onClick={onClickNewUsecaseButton}
-      />
-      <ul>
+      <h2 className={{ color: '#777777' }}>Usecases</h2>
+      <div style={{ 'text-align': 'right', 'padding-bottom': '10px' }}>
+        <button
+          className="btn btn-success"
+          onClick={onClickNewUsecaseButton}
+          style={{ 'font-weight': 'bold' }}
+        >
+          <span className="glyphicon glyphicon-plus-sign" />
+          {' Add'}
+        </button>
+      </div>
+      <div className="list-group">
         {
           usecases.map((usecase) =>
-            <li key={usecase.id}>
-              <Link to={`/usecases/${usecase.id}`}>{usecase.name}</Link>
-              <span> {(new Date(usecase.createdAt)).toLocaleString()}</span>
-            </li>
+            <Link
+              key={usecase.id}
+              to={`/usecases/${usecase.id}`}
+              className={
+                (selectedUsecaseId === usecase.id) ?
+                  'list-group-item active' : 'list-group-item'
+              }
+            >
+              <h4 className="list-group-item-heading">{usecase.name}</h4>
+              <p className="list-group-item-text">
+                created at {(new Date(usecase.createdAt)).toLocaleString()}
+              </p>
+            </Link>
           )
         }
-      </ul>
+      </div>
       <Modal name={API_NAME.ADD_USECASE} title="New Usecase">
         <NewUsecase />
       </Modal>
